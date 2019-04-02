8822f518992a269f46cbd559dfb4835fae2cf7b6
Craig Stevens <csteves44@gmail.com>
2019-04-01 22:39:51 -0600

    Make project cards flippable

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,6 +13,9 @@ class SimpleSlider extends React.Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      arrows:false,
+      autoplay:true,
+      autoplaySpeed: 3000
     }
     return (
       <Slider {...settings}>
